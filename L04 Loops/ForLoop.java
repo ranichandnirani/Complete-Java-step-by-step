04c5231ac1cdc1dfc7c9c0110c9862d9a784b804
ranichandnirani <chandnirani229@gmail.com>
2025-12-08 11:10:59 +0530

    Done

---
+++ L04 Loops/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void  main(String[] args) {
+        for(int counter = 0; counter < 5; counter++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
---
+++ L04 Loops/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        
+        int counter = 0;
+
+        while(counter < 5) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
