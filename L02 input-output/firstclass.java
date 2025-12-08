98e7b39f96314f117edff2ec6bf3dcd345982397
ranichandnirani <chandnirani229@gmail.com>
2025-12-08 10:55:11 +0530

    Start learning

--- L02 input-output/firstclass.java
+++
@@ -1,9 +0,0 @@
-class Firstclass{
-    public static void main(String[] args) {
-        System.out.print("hello");
-        System.out.println("world!");
-        System.out.print("from JAVA");
-
-
-    }
-}
--- L04 Loops/ForLoop.java
+++
@@ -1,7 +0,0 @@
-public class ForLoop {
-    public static void  main(String[] args) {
-        for(int counter = 0; counter < 5; counter++) {
-            System.out.println("Hello World!");
-        }
-    }
-}
