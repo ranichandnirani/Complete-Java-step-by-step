7ae8a7ae774bfa040ec19b7e0dfcbe2f292fb46b
ranichandnirani <chandnirani229@gmail.com>
2025-12-10 16:36:53 +0530

    Power of another number

--- L08 Functions Practice-Questions/PowerOf.java
+++ L08 Functions Practice-Questions/PowerOf.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// 8. Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
 
 public class PowerOf {
    public static void main(String[] args) {
