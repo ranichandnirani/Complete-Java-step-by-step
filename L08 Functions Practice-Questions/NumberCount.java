4d86448fadada65f15984b1332ee7159fb93f8d4
ranichandnirani <chandnirani229@gmail.com>
2025-12-10 14:15:00 +0530

    Count of positive, negative and zeros entered

---
+++ L08 Functions Practice-Questions/NumberCount.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// 5. Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered.
+public class NumberCount {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int positive = 0, negative = 0, zero = 0;
+        char choice;
+
+        do {
+            System.out.print("Enter a number: ");
+            int num = sc.nextInt();
+
+            if(num > 0) {
+                positive++;
+            } else if(num < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+
+            System.out.print("Do you want to enter another number? (y/n): ");
+            choice = sc.next().charAt(0);
+
+        } while(choice == 'y' || choice == 'Y');
+
+        System.out.println("\nCount of positive numbers: " + positive);
+        System.out.println("Count of negative numbers: " + negative);
+        System.out.println("Count of zeros: " + zero);
+    }
+}
+
