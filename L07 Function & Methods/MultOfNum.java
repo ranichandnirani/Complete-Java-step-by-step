28a84f6a8878fdd17d083df0122570086cf2fd5f
ranichandnirani <chandnirani229@gmail.com>
2025-12-10 12:54:51 +0530

    Done

--- L07 Function & Methods/MultOfNum.java
+++ L07 Function & Methods/MultOfNum.java
@@ -4,7 +4,7 @@ public class MultOfNum {
     public static int calculateProduct(int a, int b) {
         return a * b;
     }
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt();
