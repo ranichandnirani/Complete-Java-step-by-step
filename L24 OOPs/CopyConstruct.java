46732c411916e2e2c44982c7d14cc4cb73848dd9
ranichandnirani <chandnirani229@gmail.com>
2025-12-16 18:57:36 +0530

    CopyConstructor

--- L24 OOPs/CopyConstruct.java
+++ L24 OOPs/CopyConstruct.java
@@ -1,9 +1,31 @@
 class Student {
    String name;
    int age;
+
+   public void printInfo() {
+    System.out.println(this.name);
+    System.out.println(this.age);
+   }
   
    Student(Student s2) {
        this.name = s2.name;
        this.age = s2.age;
     }
+
+    Student() {
+
+    }
+ 
 } 
+public class CopyConstruct {
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Roshan";
+        s1.age = 20;
+
+        Student s2 = new Student(s1); //Copy of s1.
+        s2.printInfo();
+}
+
+}
