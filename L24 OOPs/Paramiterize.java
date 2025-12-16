f9b4b8ceb12f8ff2fd58ab7372a33d81e93082a8
ranichandnirani <chandnirani229@gmail.com>
2025-12-16 17:53:25 +0530

    Paramiterized constructore

--- L24 OOPs/Paramiterize.java
+++ L24 OOPs/Paramiterize.java
@@ -2,9 +2,20 @@ class Student {
    String name;
    int age;
   
-   Student(String name, int age) {
+   public void printInfo() {
+    System.err.println(this.name);
+    System.out.println(this.age);
+   }
+   Student(String name, int age) {   // Paramiterize constructors
        this.name = name;
        this.age = age;
    }
 }
+public class Paramiterize {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ritik", 15);
+        
 
+        s1.printInfo();
+    }
+}
