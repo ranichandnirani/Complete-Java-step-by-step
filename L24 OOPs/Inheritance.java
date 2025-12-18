ce0388b2c163f4cc287525b90dda79eb124978bd
ranichandnirani <chandnirani229@gmail.com>
2025-12-18 15:56:07 +0530

    Inheritance

---
+++ L24 OOPs/Inheritance.java
@@ -0,0 +1,14 @@
+class Shape {
+    String color;
+}
+
+class Triangle extends Shape {
+
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+    }    
+}
