a515984420ddac16bed05d7c0cb653fb3f0d50f6
ranichandnirani <chandnirani229@gmail.com>
2025-12-19 18:21:37 +0530

    Abstraction

---
+++ L24 OOPs/Abstraction.java
@@ -0,0 +1,22 @@
+abstract class Animal {
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    public void walk() {
+        System.out.println("Walk on for legs");
+    }
+}
+
+class Chicken extends Animal {
+    public void walk() {
+        System.out.println("walk on 2 legs");
+    }
+}
+public class Abstraction {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
+
