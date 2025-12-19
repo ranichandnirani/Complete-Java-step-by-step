b4c37ffa95a70db88bdb430026694cf73f3c6508
ranichandnirani <chandnirani229@gmail.com>
2025-12-19 18:28:08 +0530

    NonParamiterized Constructor

--- L24 OOPs/NonPara.java
+++ L24 OOPs/NonPara.java
@@ -1,10 +1,25 @@
-// NonoParamiterized constructor
+// NonParamiterized constructor
 
 class Student {
    String name;
    int age;
   
+   public void pritInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+   }
    Student() {
        System.out.println("Constructor called");
    }
+}
+
+public class NonPara {
+    public static void main(String[] args) {
+         
+        Student s1 = new Student();
+        s1.name = "Chandni";
+        s1.age = 21;
+
+        s1.pritInfo();
+    }
 }
