a9b1b9af67b3b6c5500cdda766fd9a72e63243ec
ranichandnirani <chandnirani229@gmail.com>
2025-12-29 16:58:14 +0530

    Print marks using Array

---
+++ L10 Arrays/DefineArr.java
@@ -0,0 +1,15 @@
+
+public class DefineArr {
+   public static void main(String args[]) {
+        int[] mark = new int[3];
+        mark[0] = 97; //phy
+        mark[1] = 98; //chem
+        mark[2] = 95;  //eng
+        
+       System.out.println(mark[0]); 
+       System.out.println(mark[1]); 
+       System.out.println(mark[2]); 
+   }
+   
+}
+
