ca4f5740ba954792d8548f8d0e8ab0b0f63a24b8
ranichandnirani <chandnirani229@gmail.com>
2026-01-01 16:28:36 +0530

    Search x taking input from user

---
+++ L11 2D-Arrays/SearchNumX.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+// Take a matrix as input from the user, Search for a given number x and print the indices at which it occurs.
+
+public class SearchNumX {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        //Inputs
+        //rows
+        for(int i=0; i<rows; i++) {
+            //cols
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        int x = sc.nextInt();
+        
+        for(int i=0; i<rows; i++) {
+           for(int j=0; j<cols; j++) {
+            // Compare with x
+            if(numbers[i][j] == x) {
+                System.out.println("x found at location (" + i +", " + j + ")");
+            }
+           } 
+        }
+    
+    }
+}
