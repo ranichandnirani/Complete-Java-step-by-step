667c4b7e82a76b60c9461772494279cc2a227881
ranichandnirani <chandnirani229@gmail.com>
2026-01-04 19:00:47 +0530

    String concatenation, length and charAt

---
+++ L12 Strings/Concatenation.java
@@ -0,0 +1,15 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        //concatenation
+       String firstName = "tony";
+       String lastName = "stark";
+       String fullName = firstName + " " + lastName;
+
+       System.out.println(fullName.length()); //for lenght
+
+       //charAt
+       for(int i=0; i<fullName.length(); i++) {
+        System.out.println(fullName.charAt(i));
+       }
+    }
+}
