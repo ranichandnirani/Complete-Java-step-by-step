f0c644073d1ca6eabe7940604d3b780ef0d6ef9f
ranichandnirani <chandnirani229@gmail.com>
2026-01-07 18:47:03 +0530

    Bit manipulation

---
+++ L15 Bit Manipulation/Bits.java
@@ -0,0 +1,20 @@
+
+public class Bits {
+     
+    public static void main(String args[]) {
+
+        //get bit
+      int n = 5; //0101
+      int pos = 3;
+      int bitMask = 1<<pos;
+
+
+      if((bitMask & n) == 0) {
+          System.out.println("bit was zero");
+      } else {
+          System.out.println("bit was one");
+      }
+
+    }
+
+}
---
+++ L15 Bit Manipulation/SetBit.java
@@ -0,0 +1,14 @@
+
+public class SetBit {
+    public static void main(String args[]) {
+      //Set bit
+        int n = 5; //0101
+      int pos = 1;
+      int bitMask = 1<<pos;
+
+
+      int newNumber = bitMask | n;
+      System.out.println(newNumber);
+   }
+
+}
