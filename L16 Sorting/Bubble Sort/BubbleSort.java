ec254253d0b93353275bc539b9985bbe828c9e47
ranichandnirani <chandnirani229@gmail.com>
2026-01-08 18:37:09 +0530

    Bubble Sort done

---
+++ L16 Sorting/Bubble Sort/BubbleSort.java
@@ -0,0 +1,25 @@
+
+class BubbleSort {
+    public static void printArray(int arr[]) {
+      for(int i=0; i<arr.length; i++) {
+        System.out.println(arr[i] +" ");
+      }  
+      System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {7, 8, 1, 2};
+
+        //bubble sort (time complexity = O(n^2))
+        for(int i=0; i<arr.length-1; i++) { // n-1
+            for(int j=0; j<arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    //Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
