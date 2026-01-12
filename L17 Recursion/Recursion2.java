c9773e6479e79bea4cc2ee29836e381beb6f08e5
ranichandnirani <chandnirani229@gmail.com>
2026-01-12 17:14:06 +0530

    Recursion

---
+++ L17 Recursion/Recursion2.java
@@ -0,0 +1,15 @@
+// print number 1 to 5
+
+public class Recursion2 {
+   public static void printNum(int n) {
+    if(n == 6) {
+        return;
+    }
+    System.out.println(n);
+    printNum(n+1);
+   }
+    public static void main(String[] args) {
+       int n = 1;
+       printNum(n);
+   } 
+}
