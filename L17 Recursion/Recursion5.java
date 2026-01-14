e9cc2ca40ffd275ece3b0392ed1033728ecbaf50
ranichandnirani <chandnirani229@gmail.com>
2026-01-14 18:59:01 +0530

    fibonacci sequence

---
+++ L17 Recursion/Recursion5.java
@@ -0,0 +1,14 @@
+// Q5. Print the fibonacci sequence till nth term.
+
+class Recursion5 {
+public static void printFactorial(int a, int b, int n) {
+if(n == 0) {
+return;
+}
+System.out.println(a);
+printFactorial(b, a+b, n-1);
+}
+public static void main(String args[]) {
+printFactorial(0, 1, 5);
+}
+}
