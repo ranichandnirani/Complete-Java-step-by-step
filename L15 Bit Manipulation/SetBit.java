76a661fc3346363efb28bcd35d2547d8e5bf4ea5
ranichandnirani <chandnirani229@gmail.com>
2026-01-23 18:54:41 +0530

    some changes

--- L15 Bit Manipulation/SetBit.java
+++ L15 Bit Manipulation/SetBit.java
@@ -1,14 +1,15 @@
 
 public class SetBit {
-    public static void main(String args[]) {
-      //Set bit
-        int n = 5; //0101
-      int pos = 1;
-      int bitMask = 1<<pos;
+  public static void main(String args[]) {
+    //Set bit
+    
+    int n = 5; //0101
+    int pos = 1;
+    int bitMask = 1<<pos;
 
+    int newNumber = bitMask | n;
+    System.out.println(newNumber);
 
-      int newNumber = bitMask | n;
-      System.out.println(newNumber);
-   }
+  }
 
 }
